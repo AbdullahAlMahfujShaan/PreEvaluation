1155c6509a073b871aac2645f72a383590d0aeec
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-07-06 17:00:43 +0600

    POST Method in Controller #1

--- spring-jpa-mysql/src/main/java/com/example/springjpamysql/controller/StudentController.java
+++ spring-jpa-mysql/src/main/java/com/example/springjpamysql/controller/StudentController.java
@@ -3,15 +3,29 @@ package com.example.springjpamysql.controller;
 import com.example.springjpamysql.entity.Student;
 import com.example.springjpamysql.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
-@Autowired
+
+    @Autowired
     private StudentService service;
 
-public Student addStudent(@RequestBody Student student){
-    
-}
+    @PostMapping("/addStudent")
+
+    public Student addStudent(@RequestBody Student student){
+        return service.saveStudent(student);
+    }
+
+
+    @PostMapping("/addStudents")
+    public List<Student> addStudent(@RequestBody List<Student> students){
+        return service.saveStudents(students);
+    }
+
+
 }
