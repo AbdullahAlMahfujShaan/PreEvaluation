0c7ac368c974be9a004bcc53dfe193c1a74d5393
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-07-06 14:49:43 +0600

    created student class #1

---
+++ spring-jpa-mysql/src/main/java/com/example/springjpamysql/entity/students.java
@@ -0,0 +1,32 @@
+package com.example.springjpamysql.entity;
+import java.util.Date;
+
+public class students {
+    private int reg_id;
+    private int institution_id;
+    private int student_id;
+    private String first_name;
+    private String last_name;
+    private Date reg_date;
+    private Date date_of_birth;
+    private int age;
+    private String gender;
+    private int nid;
+    private String occupation;
+    private String present_address;
+    private String permanent_address;
+    private String street;
+    private int post_office;
+    private String upazila;
+    private String district;
+    private String division;
+    private int contact_no;
+    private String email;
+    private int photo;
+    private Date created_on;
+    private String created_by;
+    private String degree;
+    private String institute;
+    private int completion_year;
+    private float grade;
+}
