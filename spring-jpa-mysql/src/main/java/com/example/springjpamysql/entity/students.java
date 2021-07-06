59e5f853a432036796b7bde74958b60511b1d933
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-07-06 14:53:39 +0600

    created args and constructors #1

--- spring-jpa-mysql/src/main/java/com/example/springjpamysql/entity/students.java
+++ spring-jpa-mysql/src/main/java/com/example/springjpamysql/entity/students.java
@@ -1,7 +1,24 @@
 package com.example.springjpamysql.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "STUDENT_TBL")
+
 public class students {
+
+    @Id
+    @GeneratedValue
     private int reg_id;
     private int institution_id;
     private int student_id;
